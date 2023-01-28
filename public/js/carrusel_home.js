e74a4a736456055722c3b5377b7447513e679c45
CarlosCrisonG <carloscrison@gmail.com>
2023-01-28 18:09:34 -0500

    Carrusel de imagenes agregado al home

---
+++ public/js/carrusel_home.js
@@ -0,0 +1,17 @@
+/*Script para Carrusel Home -- Inicio */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Cambiar imagen cada 4 segundos
+}
+/*Script para Carrusel Home -- Fin */
+
