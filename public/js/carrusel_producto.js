92dba13512763de1341b68cace255fa878a5ab13
lorenzoSanchez97 <lorenzo97sz@gmail.com>
2023-02-06 10:24:23 -0300

    Se agregaron algunas transiciones a registro y producto

--- public/js/carrusel_producto.js
+++ public/js/carrusel_producto.js
@@ -16,10 +16,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
