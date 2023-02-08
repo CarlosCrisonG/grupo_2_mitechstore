ac62292643e001850d68f883413ec671a673a143
CarlosCrisonG <carloscrison@gmail.com>
2023-02-08 11:33:34 -0500

    Agregados header y footer.

--- public/js/carrusel_home.js
+++ public/js/carrusel_home.js
@@ -1,17 +1,20 @@
-/*Script para Carrusel Home -- Inicio */
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Cambiar imagen cada 4 segundos
 }
-/*Script para Carrusel Home -- Fin */
-
