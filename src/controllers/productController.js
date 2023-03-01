3a17328eccfb77361a79a09ab25a926dfa9ada08
JaiderValencia <janvame05@gmail.com>
2023-03-01 16:18:29 -0500

    Hago la lista de productos dinamica

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
+
+const productsPath = path.join(__dirname, "../data/products.json");
+function getProducts() {
+  return JSON.parse(fs.readFileSync(productsPath));
+}
 
 const controller = {
   productDetail: (req, res) => {
     res.render("product/productDetail")
   },
   productList: (req, res) => {
-    res.render("product/listaDeProducto")
+    res.render("product/listaDeProducto", { products: getProducts() });
   },
   productCart: (req, res) => {
     res.render("product/productCart")
