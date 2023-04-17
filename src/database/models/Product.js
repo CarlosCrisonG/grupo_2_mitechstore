a8dd6d61d63181804936cf24e42d6d08e91a52c8
JaiderValencia <janvame05@gmail.com>
2023-04-17 11:52:02 -0500

    creo el modelo de la tabla de los products

---
+++ src/database/models/Product.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = "Product";
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(200),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL(11, 2),
+            allowNull: false
+        },
+        discount: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        highlight: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        model: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        year: {
+            type: DataTypes.INTEGER(4),
+            allowNull: false
+        },
+        size: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        weight: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        inSale: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    };
+
+    const config = {
+        timestamps: false,
+        tableName: "products"
+    };
+
+    const Product = sequelize.define(alias, cols, config);
+
+    return Product;
+}
