5a45a210e8b1020ac4a1c4707ff7afd46ca05aeb
CarlosCrisonG <carloscrison@gmail.com>
2023-04-26 05:33:42 -0500

    Added login and edit user with sequelize

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,6 +8,7 @@ const { validationResult } = require('express-validator');
 const { log } = require('console');
 
 const usersPath = path.join(__dirname, "../data/users.json");
+
 function getusers() {
   return JSON.parse(fs.readFileSync(usersPath));
 }
@@ -16,115 +17,123 @@ const controller = {
   register: (req, res) => {
     res.render("users/register");
   },
-  create: (req, res) => {
-    const errors = validationResult(req); //Form errors validation
+  create: (req, res) => { // Listo con Sequelize
+    const errors = validationResult(req);
+
     if (errors.isEmpty()) {
       db.User.findAll()
-        .then((users) => {
-          console.log(users);
-        });
-      }
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-  //     try {
-  //       const allUsers = await db.User.findAll();
-  //       console.log(allUsers);
-        
-  //       //Check if users already exists
-  //       let userInDB = allUsers.find(user => user.email == req.body.email);
-  //       if (userInDB) {
-  //         return res.render('users/register', {
-  //           errors: {
-  //             email: {
-  //               msg: "Ya existe un usuario registrado con ese email"
-  //             }
-  //           }, oldData: req.body
-  //         });
-  //       } else {
-  //         //User Creation
-  //         const userToCreate = {
-  //           // id,
-  //           first_name: req.body.first_name,
-  //           last_name: req.body.last_name,
-  //           email: req.body.email,
-  //           password: bcrypt.hashSync(req.body.password, 10),
-  //           phone: req.body.phone,
-  //           avatar,
-  //           userprofile: req.body.userprofile.toLowerCase(),
-  //           country: req.body.country,
-  //           region: req.body.region,
-  //           city: req.body.city,
-  //           zip: req.body.zip,
-  //           address: req.body.address,
-  //         };
-  //         await db.User.create(userToCreate);
-  //         res.redirect("/users/login");
-  //       }
-  //     } catch (error) {
-  //         console.log(error);
-  //         res.status(400).send("Hubo un error: " + error);
-  //     }
-  //   }
+        .then(function (users) {
+          //Check if the user is already in the database
+          const userInDB = users.find(user => user.email == req.body.email)
+          if (userInDB) {
+            return res.render('users/register', {
+              errors: {
+                email: {
+                  msg: "Ya existe un usuario registrado con ese email"
+                }
+              }, oldData: req.body
+            });
+          } else {
+            const avatar = req.file ? req.file.filename : "defaultUser.jpg";
+
+            const userToCreate = {
+              first_name: req.body.first_name,
+              last_name: req.body.last_name,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, 10),
+              phone: req.body.phone,
+              avatar,
+              region: req.body.region,
+              city: req.body.city,
+              zip: req.body.zip,
+              address: req.body.address,
+              userprofile_id: req.body.userprofile,
+              country_id: req.body.country
+            };
+            db.User.create(userToCreate);
+            res.redirect("/users/login");
+          }
+        })
+    } else {
+      res.render('users/register', {
+        errors: errors.mapped(),
+        oldData: req.body,
+      });
+    }
   },
   login: (req, res) => {
     res.render("users/login");
   },
-  processLogin: (req, res) => {
+  processLogin: (req, res) => { // Listo con Sequelize
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
 
-      const users = getusers();
-      let userToLogin = users.find((person) => person.email == req.body.email);
-
-      if (userToLogin) {
-        if (bcrypt.compareSync(req.body.password, userToLogin.password)) {
-          delete userToLogin.password;
-          req.session.userLogged = {
-            ...userToLogin,
-          };
-          return res.redirect("/");
-        } else {
-          return res.render('users/login', {
-            errors: {
-              password: {
-                msg: "La contraseña ingresada es incorrecta"
-              }
-            }, oldData: req.body
-          })
-        }
-
-      } else {
-        return res.render('users/login', {
-          errors: {
-            email: {
-              msg: "No existe ningún usuario registrado con ese email"
+      const users = db.User.findAll()
+        .then(function (users) {
+          let userToLogin = users.find((person) => person.email == req.body.email);
+          if (userToLogin) {
+            if (bcrypt.compareSync(req.body.password, userToLogin.password)) {
+              delete userToLogin.password;
+              req.session.userLogged = {
+                ...userToLogin,
+              };
+              return res.redirect("/");
+            } else {
+              return res.render('users/login', {
+                errors: {
+                  password: {
+                    msg: "La contraseña ingresada es incorrecta"
+                  }
+                }, oldData: req.body
+              })
             }
-          }, oldData: req.body
-        })
-      }
-
+          } else {
+            return res.render('users/login', {
+              errors: {
+                email: {
+                  msg: "No existe ningún usuario registrado con ese email"
+                }
+              }, oldData: req.body
+            })
+          }
+        });
     } else {
       res.render('users/login', {
         errors: errors.mapped(),
         oldData: req.body,
       });
     }
+  },
+  profile: (req, res) => { // Listo con Sequelize
+    const userInDB = db.User.findOne({
+      where: {
+        email: req.session.userLogged.dataValues.email
+      },
+    }).then(function (user) {
+      if (user) {
+        let userFound = user;
+        res.render("users/userProfile", { user: userFound });
+      } else {
+        res.send('El usuario no se encuentra en la base de datos');
+      }
+    })
 
   },
-  profile: (req, res) => {
-    res.render("users/userProfile");
-  },
-  edit: (req, res) => {
-    res.render("admin/editUser");
+  edit: (req, res) => { // Listo con Sequelize
+    const userInDB = db.User.findOne({
+      where: {
+        email: req.session.userLogged.dataValues.email
+      },
+    }).then(function (user) {
+      if (user) {
+        let userFound = user;
+        console.log(user);
+        res.render("admin/editUser", { user: userFound });
+      } else {
+        res.send('El usuario no se encuentra en la base de datos');
+      }
+    })
   },
   update: (req, res) => {
     const errors = validationResult(req);
