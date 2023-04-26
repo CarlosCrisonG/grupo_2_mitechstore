a8e151cb6164816de11793e2add0e4a9b0a27957
JaiderValencia <janvame05@gmail.com>
2023-04-26 14:16:35 -0500

    creo un seed para poblar la tabla del userprofile

---
+++ src/database/seeders/20230426191356-add_userprofiles_on_table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('userprofile', [{
+      name: 'comprador',
+    }, {
+      name: 'vendedor'
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('userprofile', null, {});
+  }
+};
