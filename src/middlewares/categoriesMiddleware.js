d9517c4a5e081cafb58bf7653148fc417d237d8b
JaiderValencia <janvame05@gmail.com>
2023-05-02 21:46:30 -0500

    corrijo el if

--- src/middlewares/categoriesMiddleware.js
+++ src/middlewares/categoriesMiddleware.js
@@ -3,10 +3,8 @@ const db = require('../database/models');
 async function categoriesMiddleware(req, res, next) {
     const categories = await db.Category.findAll();
 
-    if (categories.length != 0) {
-        res.locals.headerCategories = categories
-    };
-    
+    res.locals.headerCategories = categories
+
     next();
 }
 
