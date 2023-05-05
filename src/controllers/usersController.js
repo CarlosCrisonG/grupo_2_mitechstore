7ef361e1b75df3e6f302f19e5ab3b8c1149cefc3
CarlosCrisonG <carloscrison@gmail.com>
2023-05-05 05:44:38 -0500

    User CRUD Ready

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -9,9 +9,9 @@ const { log } = require('console');
 
 const usersPath = path.join(__dirname, "../data/users.json");
 
-function getusers() {
-  return JSON.parse(fs.readFileSync(usersPath));
-}
+// function getusers() {
+//   return JSON.parse(fs.readFileSync(usersPath));
+// }
 
 const controller = {
   register: (req, res) => {
@@ -124,30 +124,30 @@ const controller = {
     const userInDB = db.User.findOne({
       where: {
         email: req.session.userLogged.dataValues.email
-      },
+    },
     }).then(function (user) {
       if (user) {
         let userFound = user;
-        // console.log(user);
         res.render("admin/editUser", { user: userFound });
       } else {
         res.send('El usuario no se encuentra en la base de datos');
       }
     })
   },
-  update: (req, res) => {
+  update: (req, res) => { // Listo con Sequelize
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
       const userInDB = db.User.findOne({
-        where: {
-          email: req.body.email
-        },
-      }).then(function (users) {
-        console.log('TEST: ' + users[0]);
-        const userToUpdateIndex = users.findIndex(user => user.id == req.session.userLogged.id);
+        where: { email: req.body.email },
+      }).then(function (user) {
 
-        const avatar = req.file ? req.file.filename : users[userToUpdateIndex].avatar;
+        let avatar = user.avatar;
+
+        if (req.file) {
+          avatar = req.file.filename;
+          fs.unlinkSync(path.join(__dirname, "../public/images/avatars/", user.avatar));
+        } 
 
         const userToUpdate = {
           first_name: req.body.first_name,
@@ -163,104 +163,40 @@ const controller = {
           userprofile_id: req.body.userprofile,
           country_id: req.body.country
         };
-        db.User.update(userToCreate, {
-          where: {
-            id: userToUpdateIndex
-          }
+
+        db.User.update(userToUpdate, {
+          where: { id: user.id }
         });
         res.redirect("/users/userProfile");
-      })
+      });
     } else {
-      res.render('users/register', {
+      res.render('admin/editUser', {
         errors: errors.mapped(),
         oldData: req.body,
       });
     }
   },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //   if (errors.isEmpty()) {
-  //     const users = getusers()
-
-  //     const userToUpdateIndex = users.findIndex(user => user.id == req.session.userLogged.id);
-
-  //     const avatar = req.file ? req.file.filename : users[userToUpdateIndex].avatar;
-
-  //     const password = req.body.password ? bcrypt.hashSync(req.body.password, 10) : users[userToUpdateIndex].password;
-
-  //     users[userToUpdateIndex] = {
-  //       ...users[userToUpdateIndex],
-  //       first_name: req.body.first_name,
-  //       last_name: req.body.last_name,
-  //       email: req.body.email,
-  //       password,
-  //       phone: req.body.phone,
-  //       avatar,
-  //       userprofile: req.body.userprofile,
-  //       country: req.body.country,
-  //       region: req.body.region,
-  //       city: req.body.city,
-  //       zip: req.body.zip,
-  //       address: req.body.address,
-  //     }
-
-  //     fs.writeFileSync(usersPath, JSON.stringify(users, null, 3));
-
-  //     delete users[userToUpdateIndex].password;
-
-  //     req.session.userLogged = {
-  //       ...users[userToUpdateIndex]
-  //     };
-
-  //     res.redirect("/");
-  //   } else {
-  //     res.render('admin/editUser', {
-  //       errors: errors.mapped(),
-  //       oldData: req.body,
-  //     })
-  //   }
-  // },
   logout: (req, res) => { // Listo con Sequelize
     req.session.destroy();
 
     res.redirect("/");
   },
-  destroyUser: (req, res) => {
-    const users = getusers();
+  destroyUser: (req, res) => { // Listo con Sequelize
+    const userInDB = db.User.findOne({
+      where: {
+        email: req.session.userLogged.dataValues.email
+    },
+    }).then(function (user) {
+      if (user.avatar != "defaultUser.jpg") {
+        fs.unlinkSync(path.join(__dirname, "../public/images/avatars/", user.avatar));
+      }
+      db.User.destroy({ where: { id: user.id } });
 
-    const userToDestroyIndex = users.findIndex(user => user.id == req.session.userLogged.id);
-
-    if (users[userToDestroyIndex].avatar != "defaultUser.jpg") {
-      fs.unlinkSync(path.join(__dirname, "../public/images/avatars/", users[userToDestroyIndex].avatar));
-    }
-
-    users.splice(userToDestroyIndex, 1);
-
-    req.session.destroy();
-
-    fs.writeFileSync(usersPath, JSON.stringify(users, null, 3));
-
-    res.redirect("/");
+      req.session.destroy();
+      
+      res.redirect("/");
+    })
   }
 };
 
