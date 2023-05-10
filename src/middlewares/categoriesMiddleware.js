41bde62c0af96418edfa9b39eb7b2cc760fec544
JaiderValencia <janvame05@gmail.com>
2023-05-09 21:26:05 -0500

    Subo la mejora

--- src/middlewares/categoriesMiddleware.js
+++ src/middlewares/categoriesMiddleware.js
@@ -1,9 +1,13 @@
 const db = require('../database/models');
 
 async function categoriesMiddleware(req, res, next) {
-    const categories = await db.Category.findAll();
-
-    res.locals.headerCategories = categories
+    if (!req.cookies.categories) {
+        const categories = await db.Category.findAll()
+        res.cookie("categories", JSON.stringify(categories), { maxAge: 2880000000 });
+        res.locals.headerCategories = categories
+    } else {
+        res.locals.headerCategories = JSON.parse(req.cookies.categories)
+    }
 
     next();
 }
