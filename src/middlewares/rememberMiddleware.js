8db3058259e573004c71f0c90c7d4685daf1f32e
JaiderValencia <janvame05@gmail.com>
2023-05-11 15:10:34 -0500

    subo condicionales del middleware

--- src/middlewares/rememberMiddleware.js
+++ src/middlewares/rememberMiddleware.js
@@ -1,4 +1,13 @@
-function remember(req, res, next) {
+const db = require('../database/models');
+
+async function remember(req, res, next) {
+    if (req.cookies.userCookie && !req.session.userLogged) {
+        const userCookie = JSON.parse(req.cookies.userCookie);
+
+        const userInDB = await db.User.findOne({ where: { id: userCookie.id } })
+
+        userInDB ? req.session.userLogged = userCookie : null
+    }
 
     next()
 }
