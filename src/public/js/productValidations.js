6410e9e30a7728da9b5cc48e5d413a6830ac02de
JaiderValencia <janvame05@gmail.com>
2023-05-12 17:09:46 -0500

    agrego las validaciones en una función y cuando hay error se desaparece el boton de crear

--- src/public/js/productValidations.js
+++ src/public/js/productValidations.js
@@ -1,4 +1,45 @@
 window.addEventListener("load", () => {
+    function validations(field, inputField) {
+        const sigleErrorMessage = document.querySelector(`p.error-${field}`);
+
+        const errorMessage = document.createElement("p");
+
+        if (field == "name" && inputField.value.length < 5) {
+            errorMessage.classList.add("error", `error-${field}`)
+
+            errorMessage.textContent = "5 caracteres minimo"
+
+            inputField.parentNode.insertBefore(errorMessage, inputField.nextSibling);
+
+            createBtn.style.display = "none"
+        }
+
+        if (field == "description" && inputField.value.length < 20) {
+            errorMessage.classList.add("error", `error-${field}`)
+
+            errorMessage.textContent = "20 caracteres minimo"
+
+            inputField.parentNode.insertBefore(errorMessage, inputField.nextSibling);
+
+            createBtn.style.display = "none"
+        }
+
+        if (!inputField.value) {
+            errorMessage.classList.add("error", `error-${field}`)
+
+            errorMessage.textContent = "Este campo no puede estar vacio"
+
+            inputField.parentNode.insertBefore(errorMessage, inputField.nextSibling);
+
+            createBtn.style.display = "none"
+        }
+
+        if (sigleErrorMessage) {
+            sigleErrorMessage.remove()
+        }
+    }
+
+
     const fields = ["name", "description", "price", "discount", "category", "highlight", "model", "year", "size", "weight", "features"]
 
     const createBtn = document.querySelector(".create-button")
@@ -7,49 +48,7 @@ window.addEventListener("load", () => {
         const inputField = document.getElementById(field)
 
         inputField.addEventListener("focusout", () => {
-            const sigleErrorMessage = document.querySelector(`p.error-${field}`);
-
-            const errorMessage = document.createElement("p");
-
-            if (field == "name" && inputField.value.length < 5) {
-                errorMessage.classList.add("error", `error-${field}`)
-
-                errorMessage.textContent = "5 caracteres minimo"
-
-                inputField.parentNode.insertBefore(errorMessage, inputField.nextSibling);
-
-                createBtn.addEventListener("click", (e) => {
-                    e.preventDefault();
-                })
-            }
-
-            if (field == "description" && inputField.value.length < 20) {
-                errorMessage.classList.add("error", `error-${field}`)
-
-                errorMessage.textContent = "20 caracteres minimo"
-
-                inputField.parentNode.insertBefore(errorMessage, inputField.nextSibling);
-
-                createBtn.addEventListener("click", (e) => {
-                    e.preventDefault();
-                })
-            }
-
-            if (!inputField.value) {
-                errorMessage.classList.add("error", `error-${field}`)
-
-                errorMessage.textContent = "Este campo no puede estar vacio"
-
-                inputField.parentNode.insertBefore(errorMessage, inputField.nextSibling);
-
-                createBtn.addEventListener("click", (e) => {
-                    e.preventDefault();
-                })
-            }
-
-            if (sigleErrorMessage) {
-                sigleErrorMessage.remove()
-            }
-        })
+            validations(field, inputField);
+        })        
     })
 })
