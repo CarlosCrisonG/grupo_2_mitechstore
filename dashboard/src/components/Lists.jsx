836f310b3ac203cab9660586b8a51aef02b63e7f
CarlosCrisonG <carloscrison@gmail.com>
2023-05-26 20:52:46 -0500

    React installed and configured

---
+++ dashboard/src/components/Lists.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Lists extends React.Component {
+    render () {
+        return (
+            <>
+            <h1>Lists</h1>
+            </>
+        )
+    }
+    
+}
+
+export default Lists;
