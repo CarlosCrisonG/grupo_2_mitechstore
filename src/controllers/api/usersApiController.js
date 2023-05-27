6415d0be08d075900d37a6bd2129b739bb1931b3
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-05-27 17:44:01 -0300

    users api detail, done

--- src/controllers/api/usersApiController.js
+++ src/controllers/api/usersApiController.js
@@ -4,11 +4,7 @@ const controller = {
     list: async (req, res) => {
         try {
             let usersFromDb = await db.User.findAll({
-                attributes: { exclude: ["password", "country_id", "userprofile_id"] },
-                include: [
-                    { association: "userProfile" },
-                    { association: "country" }
-                ]
+                attributes: { exclude: ["password", "country_id", "userprofile_id", "phone", "region", "city", "zip", "address"] }
             });
 
             let data = usersFromDb.map(user => ({
@@ -23,6 +19,31 @@ const controller = {
                     url: req.originalUrl
                 },
                 data: data
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error)
+        }
+    },
+    detail: async (req, res) => {
+        try {
+            let user = await db.User.findOne({
+                where: {
+                    id: req.params.id
+                },
+                attributes: { exclude: ["password", "country_id", "userprofile_id"] },
+                include: [
+                    { association: "userProfile" },
+                    { association: "country" }
+                ]
+            })
+            res.json({
+                meta: {
+                    status: 200,
+                    url: req.originalUrl
+                },
+                data: { ...user.dataValues, avatarURL: `/images/avatars/${user.dataValues.avatar}` }
+
             })
         } catch (error) {
             console.log(error);
--- src/routes/api/usersApiRoutes.js
+++ src/routes/api/usersApiRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const usersApiController = require("../../controllers/api/usersApiController")
 
 router.get("/", usersApiController.list);
-// router.get("/:id", usersApiController.detail)
+router.get("/:id", usersApiController.detail)
 
 module.exports = router;
