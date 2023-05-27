7eb6eeca48b9cc7c6adf4cd4f29340fe9cfe92b7
JaiderValencia <janvame05@gmail.com>
2023-05-27 18:05:24 -0500

    ya se trae todos los productos de la API y tiene paginado

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -1,6 +1,40 @@
+const db = require('../../database/models');
+
 const controller = {
-    list: (req, res) => {
-        res.send("Hello world")
+    list: async (req, res) => {
+        try {
+            const limit = 10;
+
+            const pag = parseInt(req.query.pag) ? parseInt(req.query.pag) : 1
+
+            const offset = limit * (pag - 1)
+
+            const products = await db.Product.findAll({ limit, offset, include: { all: true } })
+
+            const jsonRes = {
+                meta: {
+                    status: 200,
+                    count: products.length,
+                },
+                data: products
+            }
+
+            if (req.query.pag > 1 && products.length == 10) {
+                jsonRes.meta.next = "http://localhost:3000/api/products/?pag=" + (parseInt(req.query.pag) + 1)
+            }
+
+            if (req.query.pag > 1) {
+                jsonRes.meta.previous = "http://localhost:3000/api/products/?pag=" + (parseInt(req.query.pag) - 1)
+            }
+
+            res.status(200).json(jsonRes)
+        } catch (error) {
+            res.status(400).json({
+                status: 400,
+                msg: "Bad request, try later...",
+                errorMsg: error
+            })
+        }
     }
 }
 
