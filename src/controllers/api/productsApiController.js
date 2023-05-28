464e43f34fa534305d8348f2a6b0a7c01d052a0d
JaiderValencia <janvame05@gmail.com>
2023-05-27 23:31:17 -0500

    Agrego la url de las imagenes al list de la Api de productos

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -2,40 +2,40 @@ const db = require('../../database/models');
 
 const controller = {
     list: async (req, res) => {
-        try {
-            const limit = 10;
+        
+        const limit = 10;
 
-            const pag = parseInt(req.query.pag) ? parseInt(req.query.pag) : 1
+        const pag = parseInt(req.query.pag) ? parseInt(req.query.pag) : 1
 
-            const offset = limit * (pag - 1)
+        const offset = limit * (pag - 1)
 
-            const products = await db.Product.findAll({ limit, offset, include: { all: true } })
+        const products = await db.Product.findAll({ limit, offset, include: { all: true } })
 
-            const jsonRes = {
-                meta: {
-                    status: 200,
-                    count: products.length,
-                    url: req.originalUrl
-                },
-                data: products
-            }
+        const productsWithUrlImage = products.map(product => {
+            const url = product.images.map(image => ({url: "http://localhost:3000/images/products/" + image.name }))
 
-            if (req.query.pag > 1 && products.length == 10) {
-                jsonRes.meta.next = "http://localhost:3000/api/products/?pag=" + (parseInt(req.query.pag) + 1)
-            }
+            return { product, imagesUrl: url }
+        })
 
-            if (req.query.pag > 1) {
-                jsonRes.meta.previous = "http://localhost:3000/api/products/?pag=" + (parseInt(req.query.pag) - 1)
-            }
-
-            res.status(200).json(jsonRes)
-        } catch (error) {
-            res.status(400).json({
-                status: 400,
-                msg: "Bad request, try later...",
-                errorMsg: error
-            })
+        const jsonRes = {
+            meta: {
+                status: 200,
+                count: productsWithUrlImage.length,
+                url: req.originalUrl
+            },
+            data: productsWithUrlImage
         }
+
+        if (req.query.pag > 1 && productsWithUrlImage.length == 10) {
+            jsonRes.meta.next = "http://localhost:3000/api/products/?pag=" + (parseInt(req.query.pag) + 1)
+        }
+
+        if (req.query.pag > 1) {
+            jsonRes.meta.previous = "http://localhost:3000/api/products/?pag=" + (parseInt(req.query.pag) - 1)
+        }
+
+        res.status(200).json(jsonRes)
+
     },
     detail: async (req, res) => {
 
@@ -60,7 +60,7 @@ const controller = {
 
             jsonRes.data = "product not found"
         }
-        
+
         res.status(statusRes).json(jsonRes);
     }
 }
