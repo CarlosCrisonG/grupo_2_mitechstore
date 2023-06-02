36b082efe21714c6c3e35446a9bea9221e3eff3b
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-06-02 12:37:41 -0300

    Actualizo los scripts de estructura y datos

--- src/database/SQL/data.sql
+++ src/database/SQL/data.sql
@@ -1,124 +1,150 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: mitechstore.c7kociemno1b.us-east-2.rds.amazonaws.com    Database: mi_tech_store
--- ------------------------------------------------------
--- Server version	8.0.32
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2023 a las 16:38:29
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- GTID state at the beginning of the backup 
+-- Base de datos: `mi_tech_store`
 --
 
 --
--- Dumping data for table `categories`
+-- Volcado de datos para la tabla `categories`
 --
 
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'computadoras'),(2,'celulares'),(3,'accesorios'),(4,'electrodomesticos'),(5,'cuidado personal');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Computadoras'),
+(2, 'Celulares'),
+(3, 'Accesorios'),
+(4, 'Electrodomesticos'),
+(5, 'Cuidado personal');
 
 --
--- Dumping data for table `colors`
+-- Volcado de datos para la tabla `colors`
 --
 
-LOCK TABLES `colors` WRITE;
-/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
-INSERT INTO `colors` VALUES (1,'negro'),(2,'blanco'),(3,'gris'),(4,'rojo'),(5,'verde'),(6,'azul'),(7,'amarillo'),(8,'violeta'),(9,'rosado'),(10,'anaranjado');
-/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `colors` (`id`, `name`) VALUES
+(1, 'negro'),
+(2, 'blanco'),
+(3, 'gris'),
+(4, 'rojo'),
+(5, 'verde'),
+(6, 'azul'),
+(7, 'amarillo'),
+(8, 'violeta'),
+(9, 'rosado'),
+(10, 'anaranjado');
 
 --
--- Dumping data for table `colors_has_products`
+-- Volcado de datos para la tabla `countries`
 --
 
-LOCK TABLES `colors_has_products` WRITE;
-/*!40000 ALTER TABLE `colors_has_products` DISABLE KEYS */;
-INSERT INTO `colors_has_products` VALUES (1,1),(1,2),(1,3);
-/*!40000 ALTER TABLE `colors_has_products` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'Argentina'),
+(2, 'Bolivia'),
+(3, 'Brasil'),
+(4, 'Chile'),
+(5, 'Colombia'),
+(6, 'Ecuador'),
+(7, 'Paraguay'),
+(8, 'Peru'),
+(9, 'Uruguay'),
+(10, 'Venezuela');
 
 --
--- Dumping data for table `countries`
+-- Volcado de datos para la tabla `products`
 --
 
-LOCK TABLES `countries` WRITE;
-/*!40000 ALTER TABLE `countries` DISABLE KEYS */;
-INSERT INTO `countries` VALUES (1,'Arg'),(2,'Bol'),(3,'Bra'),(4,'Chi'),(5,'Col'),(6,'Ecu'),(7,'Par'),(8,'Per'),(9,'Uru'),(10,'Ven');
-/*!40000 ALTER TABLE `countries` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `discount`, `highlight`, `model`, `year`, `size`, `weight`, `inSale`, `categories_id`) VALUES
+(1, 'Xiaomi Watch S1 Active', 'Pantalla HD de 1.43 pulgadas con mas de 200 pantallas de reloj. Diseño personalizable', '151.00', 10, 1, 'active', 2020, '46.5 x 47.3 x 11 mm', '36.3 g', 1, 3),
+(2, 'Xiaomi Watch S1', 'Pantalla HD de 1.43 pulgadas. Cristal de zafiro. Caja de acero inoxidable. Correa de cuero', '205.00', 10, 1, 'active', 2019, '46.5 x 47.3 x 11 mm', '36.3 g', 1, 3),
+(3, 'Vacuum-Mop 2 Pro', 'Con trapeado de vibración sónica de alta frecuencia.', '510.00', 50, 0, 'active', 2019, '13.9 x 13.78 x 3.21 inches', '36.3 g', 1, 3);
 
 --
--- Dumping data for table `features`
+-- Volcado de datos para la tabla `colors_has_products`
 --
 
-LOCK TABLES `features` WRITE;
-/*!40000 ALTER TABLE `features` DISABLE KEYS */;
-INSERT INTO `features` VALUES (1,'Pantalla AMOLED HD de 1.43',1),(2,'117 Modos fitness',1),(3,'Dual-band GPS',1),(4,'Monitoreo All-day',1),(5,'12 días de duración de batería',1),(6,'Una gran pantalla AMOLED de 1,43',2),(7,'El cristal de zafiro* es extremadamente duro y resistente al desgaste',2),(8,'El Xiaomi Watch S1 incluye dos correas: una de cuero de primera calidad y una de fluorocarbono resistente a la suciedad.',2),(9,'Toda la unidad vibra para aumentar significativamente el poder de limpieza',3),(10,'Cuenta con microfibras para garantizar una penetración uniforme del agua',3),(11,'Las migas se aspiran en un instante',3);
-/*!40000 ALTER TABLE `features` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `colors_has_products` (`colors_id`, `products_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3);
 
 --
--- Dumping data for table `images`
+-- Volcado de datos para la tabla `features`
 --
 
-LOCK TABLES `images` WRITE;
-/*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (1,'xiaomi-reloj1-id1.jpg',1),(2,'xiaomi-reloj2-id1.jpg',1),(3,'xiaomi-reloj3-id1.jpg',1),(4,'xiaomi-reloj4-id1.jpg',1),(5,'xiaomi-reloj1-id2.png',2),(6,'xiaomi-reloj-id2.png',2),(7,'vacum1-id3.png',3),(8,'vacum2-id3.png',3),(9,'vacum3-id3.png',3);
-/*!40000 ALTER TABLE `images` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `features` (`id`, `name`, `products_id`) VALUES
+(1, 'Pantalla AMOLED HD de 1.43', 1),
+(2, '117 Modos fitness', 1),
+(3, 'Dual-band GPS', 1),
+(4, 'Monitoreo All-day', 1),
+(5, '12 días de duración de batería', 1),
+(6, 'Una gran pantalla AMOLED de 1,43', 2),
+(7, 'El cristal de zafiro* es extremadamente duro y resistente al desgaste', 2),
+(8, 'El Xiaomi Watch S1 incluye dos correas: una de cuero de primera calidad y una de fluorocarbono resistente a la suciedad.', 2),
+(9, 'Toda la unidad vibra para aumentar significativamente el poder de limpieza', 3),
+(10, 'Cuenta con microfibras para garantizar una penetración uniforme del agua', 3),
+(11, 'Las migas se aspiran en un instante', 3);
 
 --
--- Dumping data for table `products`
+-- Volcado de datos para la tabla `images`
 --
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Xiaomi Watch S1 Active','Pantalla HD de 1.43 pulgadas con mas de 200 pantallas de reloj. Diseño personalizable',151.00,10,1,'active',2020,'46.5 x 47.3 x 11 mm','36.3 g',1,3),(2,'Xiaomi Watch S1','Pantalla HD de 1.43 pulgadas. Cristal de zafiro. Caja de acero inoxidable. Correa de cuero',205.00,10,1,'active',2019,'46.5 x 47.3 x 11 mm','36.3 g',1,3),(3,'Vacuum-Mop 2 Pro','Con trapeado de vibración sónica de alta frecuencia.',510.00,50,0,'active',2019,'13.9 x 13.78 x 3.21 inches','36.3 g',1,3);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `images` (`id`, `name`, `products_id`) VALUES
+(1, 'xiaomi-reloj1-id1.jpg', 1),
+(2, 'xiaomi-reloj2-id1.jpg', 1),
+(3, 'xiaomi-reloj3-id1.jpg', 1),
+(4, 'xiaomi-reloj4-id1.jpg', 1),
+(5, 'xiaomi-reloj1-id2.png', 2),
+(6, 'xiaomi-reloj-id2.png', 2),
+(7, 'vacum1-id3.png', 3),
+(8, 'vacum2-id3.png', 3),
+(9, 'vacum3-id3.png', 3);
 
 --
--- Dumping data for table `userprofile`
+-- Volcado de datos para la tabla `userprofile`
 --
 
-LOCK TABLES `userprofile` WRITE;
-/*!40000 ALTER TABLE `userprofile` DISABLE KEYS */;
-INSERT INTO `userprofile` VALUES (1,'Comprador'),(2,'Vendedor'),(3,'Administrador');
-/*!40000 ALTER TABLE `userprofile` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `userprofile` (`id`, `name`) VALUES
+(1, 'Comprador'),
+(2, 'Vendedor'),
+(3, 'Administrador');
 
 --
--- Dumping data for table `users`
+-- Volcado de datos para la tabla `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'John','Doe','johndoe@gmail.com','$2a$10$aMYLt2jnLEEQqP6kc2DOAOWAOSqv.l50TrDXVMUpIslN37rvmAw8G','149598249','defaultUser.jpg','Antioquia','Medellín','2700','calle falsa 123',1,5),(2,'Jorge','Rodríguez','jorgito@gmail.com','$2a$10$RYPAxH7L9lp7jm9FxMyOY.T05J9Fo2tbx3U/gvv.O7fctgJWRlVgO','176815218','defaultUser.jpg','Rio Negro','Cipolletti','8326','calle falsa 678',2,1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `phone`, `avatar`, `region`, `city`, `zip`, `address`, `userprofile_id`, `country_id`) VALUES
+(1, 'John', 'Doe', 'johndoe@gmail.com', '$2a$10$aMYLt2jnLEEQqP6kc2DOAOWAOSqv.l50TrDXVMUpIslN37rvmAw8G', '149598249', 'defaultUser.jpg', 'Antioquia', 'Medellín', '2700', 'calle falsa 123', 1, 5),
+(2, 'Jorge', 'Rodríguez', 'jorgito@gmail.com', '$2a$10$RYPAxH7L9lp7jm9FxMyOY.T05J9Fo2tbx3U/gvv.O7fctgJWRlVgO', '176815218', 'defaultUser.jpg', 'Rio Negro', 'Cipolletti', '8326', 'calle falsa 678', 2, 1),
+(3, 'Juan', 'Pérez', 'juan.perez@example.com', '$2a$10$uieRfLHEFIRtMjQtvFuq5.XZ3RQfZVlVKB7B8v3vXuq2HtB9hKsUu', '123456789', 'defaultUser.jpg', 'Antioquia', 'Medellín', '2700', 'calle falsa 123', 1, 1),
+(4, 'María', 'López', 'maria.lopez@example.com', '$2a$10$ds6P4b1R2R.s6Yer2O7jq.UXDAI0dmeAdgRIZzn6fyWZ5OnlCMC7q', '987654321', 'defaultUser.jpg', 'Rio Negro', 'Cipolletti', '8326', 'calle falsa 456', 2, 2),
+(5, 'Luis', 'González', 'luis.gonzalez@example.com', '$2a$10$KjZId4Nedf8X0r5f8v2Qsul.e1H6.c3S0YCV45z8sV0BEIw4sxSyq', '789456123', 'defaultUser.jpg', 'Buenos Aires', 'La Plata', '1900', 'calle falsa 789', 1, 3),
+(6, 'Ana', 'García', 'ana.garcia@example.com', '$2a$10$LiXaSRrcrFTXBkZ5HIEc/.ma3g7.WBOeDv8Pl5xwT2/M5WE2I9bc.', '654321987', 'defaultUser.jpg', 'São Paulo', 'São Paulo', '4534', 'calle falsa 987', 2, 4),
+(7, 'Carlos', 'Martínez', 'carlos.martinez@example.com', '$2a$10$K4RvhugyzSYGqtbwWun44.l2vmwOSU.xEDiG5aY3AxyY.DSN5YIkC', '987123654', 'defaultUser.jpg', 'Antioquia', 'Envigado', '2300', 'calle falsa 654', 1, 5),
+(8, 'Andrés', 'Gómez', 'andres.gomez@example.com', '$2a$10$ZxkjB0x.Pjz0xqqjg8QRIOfe5NOXj2V86AbL2QoOhIiooO.GXrlEa', '987654321', 'defaultUser.jpg', 'Valparaíso', 'Viña del Mar', '2520', 'calle falsa 456', 2, 2),
+(9, 'María', 'Sánchez', 'maria.sanchez@example.com', '$2a$10$3UMtkQsvx66dN8Z7edS7ouYoefv.zfMXLHgsCWeTCI4J2pVRW9s3m', '789456123', 'defaultUser.jpg', 'Buenos Aires', 'Mar del Plata', '7600', 'calle falsa 789', 1, 3),
+(10, 'Sofía', 'López', 'sofia.lopez@example.com', '$2a$10$L/.u5jxiz9zsyHQBEWZUXeYp.ZqNopZBc4Dg7f0.GVyU/D1sRMT6.', '654321987', 'defaultUser.jpg', 'São Paulo', 'Campinas', '1300', 'calle falsa 987', 2, 4),
+(11, 'Laura', 'Hernández', 'laura.hernandez@example.com', '$2a$10$tU7ZGvB0JQpVtx8I7q2Cl.kdy8PA4ncrBpMFOj.7AL/q/xxSp2WGS', '456789123', 'defaultUser.jpg', 'San José', 'San José', '1010', 'calle falsa 321', 2, 8),
+(12, 'Diego', 'Rodríguez', 'diego.rodriguez@example.com', '$2a$10$CpEeZy2lfWZpUfJw21Y6ae6ZdJhQPEgMHCGyVYXbRjWBGkVZdL4Ou', '987654321', 'defaultUser.jpg', 'Valparaíso', 'Valparaíso', '2340', 'calle falsa 111', 1, 6),
+(13, 'Fernanda', 'Martínez', 'fernanda.martinez@example.com', '$2a$10$OwKfABzhQxyQew6Ge7K5Wubu1vWxJe.lZxguVKQp9Z/tKG/nrgSFS', '789456123', 'defaultUser.jpg', 'Buenos Aires', 'Bahía Blanca', '8000', 'calle falsa 222', 2, 7),
+(14, 'Carlos', 'Fernández', 'carlos.fernandez@example.com', '$2a$10$BpExeZC3/EJdyVQ4iv.4LOt0R1yz/VXv7aOJqSgk3IRghv1G6FS8K', '654321987', 'defaultUser.jpg', 'São Paulo', 'Sorocaba', '1000', 'calle falsa 333', 1, 8),
+(15, 'Mariana', 'López', 'mariana.lopez@example.com', '$2a$10$G3AS5EkWpl9bk0bACqlVgeYz3sE9dRHGdFtWOFABmBw2fcTVt5nRq', '987123654', 'defaultUser.jpg', 'Antioquia', 'Itagüí', '0540', 'calle falsa 444', 2, 9);
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-05-05 20:12:24
--- src/database/SQL/structure.sql
+++ src/database/SQL/structure.sql
@@ -1,202 +1,307 @@
-CREATE DATABASE  IF NOT EXISTS `mi_tech_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `mi_tech_store`;
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: mitechstore.c7kociemno1b.us-east-2.rds.amazonaws.com    Database: mi_tech_store
--- ------------------------------------------------------
--- Server version	8.0.32
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2023 a las 16:49:11
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- GTID state at the beginning of the backup 
+-- Base de datos: `mi_tech_store`
+--
+CREATE DATABASE IF NOT EXISTS `mi_tech_store` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mi_tech_store`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
 --
 
--- SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
-
---
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `colors`
+-- Estructura de tabla para la tabla `colors`
 --
 
-DROP TABLE IF EXISTS `colors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `colors` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `colors_has_products`
+-- Estructura de tabla para la tabla `colors_has_products`
 --
 
-DROP TABLE IF EXISTS `colors_has_products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `colors_has_products` (
-  `colors_id` int NOT NULL,
-  `products_id` int NOT NULL,
-  PRIMARY KEY (`colors_id`,`products_id`),
-  KEY `fk_colors_has_products_products1_idx` (`products_id`),
-  KEY `fk_colors_has_products_colors1_idx` (`colors_id`),
-  CONSTRAINT `fk_colors_has_products_colors1` FOREIGN KEY (`colors_id`) REFERENCES `colors` (`id`),
-  CONSTRAINT `fk_colors_has_products_products1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `colors_id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `countries`
+-- Estructura de tabla para la tabla `countries`
 --
 
-DROP TABLE IF EXISTS `countries`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `countries` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(3) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `features`
+-- Estructura de tabla para la tabla `features`
 --
 
-DROP TABLE IF EXISTS `features`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `features` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
-  `products_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_features_products1_idx` (`products_id`),
-  CONSTRAINT `fk_features_products1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `products_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `images`
+-- Estructura de tabla para la tabla `images`
 --
 
-DROP TABLE IF EXISTS `images`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `images` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` text CHARACTER SET utf8mb4 NOT NULL,
-  `products_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_images_products_idx` (`products_id`),
-  CONSTRAINT `fk_images_products` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `products_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Estructura de tabla para la tabla `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `description` text NOT NULL,
   `price` decimal(11,2) NOT NULL,
-  `discount` int DEFAULT '0',
-  `highlight` tinyint NOT NULL,
+  `discount` int(11) DEFAULT 0,
+  `highlight` tinyint(4) NOT NULL,
   `model` varchar(100) NOT NULL,
-  `year` int NOT NULL,
+  `year` int(11) NOT NULL,
   `size` varchar(100) NOT NULL,
   `weight` varchar(45) NOT NULL,
-  `inSale` tinyint NOT NULL,
-  `categories_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_products_categories1_idx` (`categories_id`),
-  CONSTRAINT `fk_products_categories1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `inSale` tinyint(4) NOT NULL,
+  `categories_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `userprofile`
+-- Estructura de tabla para la tabla `userprofile`
 --
 
-DROP TABLE IF EXISTS `userprofile`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `userprofile` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estructura de tabla para la tabla `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
   `password` text NOT NULL,
   `phone` text NOT NULL,
-  `avatar` text CHARACTER SET utf8mb4 NOT NULL,
+  `avatar` text NOT NULL,
   `region` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
   `zip` varchar(45) NOT NULL,
   `address` text NOT NULL,
-  `userprofile_id` int NOT NULL,
-  `country_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  KEY `fk_users_userprofile1_idx` (`userprofile_id`),
-  KEY `fk_users_country1_idx` (`country_id`),
-  CONSTRAINT `fk_users_country1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`),
-  CONSTRAINT `fk_users_userprofile1` FOREIGN KEY (`userprofile_id`) REFERENCES `userprofile` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
-/*!40101 SET character_set_client = @saved_cs_client */;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  `userprofile_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `colors`
+--
+ALTER TABLE `colors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `colors_has_products`
+--
+ALTER TABLE `colors_has_products`
+  ADD PRIMARY KEY (`colors_id`,`products_id`),
+  ADD KEY `fk_colors_has_products_products1_idx` (`products_id`),
+  ADD KEY `fk_colors_has_products_colors1_idx` (`colors_id`);
+
+--
+-- Indices de la tabla `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `features`
+--
+ALTER TABLE `features`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_features_products1_idx` (`products_id`);
+
+--
+-- Indices de la tabla `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_images_products_idx` (`products_id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_products_categories1_idx` (`categories_id`);
+
+--
+-- Indices de la tabla `userprofile`
+--
+ALTER TABLE `userprofile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD KEY `fk_users_userprofile1_idx` (`userprofile_id`),
+  ADD KEY `fk_users_country1_idx` (`country_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `colors`
+--
+ALTER TABLE `colors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `features`
+--
+ALTER TABLE `features`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `userprofile`
+--
+ALTER TABLE `userprofile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `colors_has_products`
+--
+ALTER TABLE `colors_has_products`
+  ADD CONSTRAINT `fk_colors_has_products_colors1` FOREIGN KEY (`colors_id`) REFERENCES `colors` (`id`),
+  ADD CONSTRAINT `fk_colors_has_products_products1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
+
+--
+-- Filtros para la tabla `features`
+--
+ALTER TABLE `features`
+  ADD CONSTRAINT `fk_features_products1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
+
+--
+-- Filtros para la tabla `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `fk_images_products` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_products_categories1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_country1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`),
+  ADD CONSTRAINT `fk_users_userprofile1` FOREIGN KEY (`userprofile_id`) REFERENCES `userprofile` (`id`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-04-20 21:56:06
